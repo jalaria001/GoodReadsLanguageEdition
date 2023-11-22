dc6ea9f7bd17c167606d042dda22a061ae6ac362
jalaria001 <jalaria001@gmail.com>
2023-11-22 18:02:09 +0100

    añadida vista en tablas

--- GoodReadsLngExt.user.js
+++ GoodReadsLngExt.user.js
@@ -22,6 +22,7 @@ var debug = function(x) {
 };
 
 window.onload = () => {
+
   var currentUrl = window.location.href;
     //https://www.goodreads.com/book/show/18007564-the-martian?from_search=true&from_srp=true&qid=DJKxvjpoXZ&rank=1
     var match = currentUrl.match(/goodreads.com\/book\/show\/(\d+)/);
@@ -34,20 +35,24 @@ window.onload = () => {
       {
           var status = $1("<div id='editiCheckingStatus'><img alt='' src='https://raw.githubusercontent.com/Marvel999/Android-Loading-Animation/master/LoadingAnimation/src/main/res/drawable/triad_ring.gif'  width='16' height='16'> Checking editions...</div>");
           $1(divBookActions).append(status);
-          getBookDetail(divBookActions, "https://www.goodreads.com/book/show/"+bookId)
+          getBookDetail(divBookActions, "https://www.goodreads.com/book/show/"+bookId, false)
       }
   }
 };
 
 var image = "<img style='margin: 0px 0px 0px 7px' alt='' src='https://images.freeimages.com/fic/images/icons/662/world_flag/256/flag_of_czech_republic.png' width='23' height='15' class='thumbborder' >";
 var loadingimage = "<img alt='' src='http://forum.xda-developers.com/clientscript/loading_small.gif'  width='16' height='16'>";
-var getEditions = function(ref, data) {
+var getEditions = function(ref, data, isEntabla) {
                 var status = $1($1(ref).find('#editiCheckingStatus'));
 
                 var editionData = $1($1($1($1(data).find("div.editionData")).has("div.dataValue:contains('" + language + "'):first")).find("a.bookTitle"));
                 var imageData = $1($1($1($1(data).find("div.elementList")).has("div.dataValue:contains('" + language + "'):first")).find("img"));
                 if (editionData !== undefined && editionData.exists()) {
-                    var langDivContainer = $1("<div class='langEditionContainer' style='overflow-y: auto; max-height: 400px; margin-top: 10px;'></div>");
+                    if(isEntabla){
+                        var langDivContainer = $1("<div class='langEditionContainer' style='overflow-x: auto; max-height: 74px; max-width: 150px; margin-top: 10px; '></div>");
+                    }else{
+                        var langDivContainer = $1("<div class='langEditionContainer' style='overflow-y: auto; max-height: 400px; margin-top: 10px;'></div>");
+                    }
                     debugger ;
                     if (status !== undefined)
                         status.remove();
@@ -84,7 +89,7 @@ var getEditions = function(ref, data) {
 
             }
 
-var getBookDetail = function(ref, url) {
+var getBookDetail = function(ref, url, isEntabla) {
     $1.get(url).success(function(bookDetail) {
         //extract
 
@@ -96,7 +101,7 @@ var getBookDetail = function(ref, url) {
             link = match[0];
             debug("getting edition details for "+ link);
             $1.get(link+"?utf8=✓&per_page=10000&expanded=true").success(function(data) {
-                getEditions(ref, data);
+                getEditions(ref, data, isEntabla);
             }).error(function(jqXHR, textStatus, errorThrown) {
                 debug("error:"+textStatus+" "+errorThrown);
             });
@@ -110,13 +115,13 @@ $1(".wtrButtonContainer").each(function() {
 
      var status = $1("<div id='editiCheckingStatus'><img alt='' src='https://raw.githubusercontent.com/Marvel999/Android-Loading-Animation/master/LoadingAnimation/src/main/res/drawable/triad_ring.gif'  width='16' height='16'> Checking editions...</div>");
     var element = $1(this);
-	var parentTd = element.closest("td:has(a.bookTitle)");
+	var parentTd = element.closest("tr:has(a.bookTitle)");
   if (parentTd.length > 0) {
     element = parentTd;
   }
-    element.height("+=35");
+    //element.height("+=30");
      element.append(status);
     var bookId = $1(element.find('input#book_id:first')).attr('value');
-    getBookDetail(element, "https://www.goodreads.com/book/show/"+bookId)
+    getBookDetail(element, "https://www.goodreads.com/book/show/"+bookId, true);
 
 });
